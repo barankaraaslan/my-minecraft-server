5efc2e9802cb2af2f84fc5708c16acba36f6bc86
Baran Karaaslan <barankaraaslan_06@hotmail.com>
2022-02-08 23:20:29 +0300

    add tags to identify vpc resouces from console

--- main.tf
+++ main.tf
@@ -23,15 +23,24 @@ provider "aws" {
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  tags {
+    Name = "my-minecraft-server"
+  }
 }
 
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
+  tags {
+    Name = "my-minecraft-server"
+  }
 }
 
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
+  tags {
+    Name = "my-minecraft-server"
+  }
 }
 
 resource "aws_ecs_cluster" "main" {

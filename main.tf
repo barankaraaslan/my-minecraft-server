d7b921782a4c251a1996d7cbab8f8276706e509a
Baran Karaaslan <barankaraaslan_06@hotmail.com>
2022-02-08 23:28:37 +0300

    create a handmade routing tabl

--- main.tf
+++ main.tf
@@ -35,6 +35,18 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = aws_vpc.main.cidr_block
+  }
+
+  tags = {
+    Name = "my-minecraft-server"
+  }
+}
+
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"

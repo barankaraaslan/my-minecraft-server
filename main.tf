7ac051a309e82384c3cd4a7f23591600d162885b
Baran Karaaslan <developer.barankaraaaslan@outlook.com>
2022-02-08 16:43:16 +0300

    add ecs stuff and rename vpc

--- main.tf
+++ main.tf
@@ -24,11 +24,49 @@ provider "aws" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "main"
+  name = "my-minecraft-server"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
+}
+
+resource "aws_ecs_cluster" "main" {
+  name = "my-minecraft-server"
+}
+
+resource "aws_ecs_task_definition" "main" {
+  family                   = "my-minecraft-server"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "512"
+  memory                   = "1024"
+  container_definitions = <<DEFINITION
+[
+  {
+    "cpu":0,
+    "dnsSearchDomains":[],
+    "dnsServers":[],
+    "dockerLabels":{},
+    "dockerSecurityOptions":[],
+    "essential":true,
+    "extraHosts":[],
+    "image": "nginx",
+    "links":[],
+    "mountPoints":[],
+    "name": "nginx-test",
+    "portMappings":[
+      {
+        "containerPort": 80,
+        "hostPort":80,
+        "protocol": "tcp"
+      }
+    ],
+    "ulimits":[],
+    "volumesFrom":[],
+  }
+]
+DEFINITION
 }

491783ca094515099050a8c505df87393fefbe2e
Baran Karaaslan <developer.barankaraaaslan@outlook.com>
2022-02-09 08:22:04 +0300

    change subnet ip range to eliminate differences with manually created test subnets

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "aws_route_table" "main" {
 
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.0.0/24"
   tags = {
     Name = "my-minecraft-server"
   }
